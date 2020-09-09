1a0d7b8321f23b0fc3c04f5978c4c4a334ca86a0
GabrielIFPB <jeronimogbariel@gmail.com>
2020-09-09 14:05:51 -0300

    Algoritmo de ordenação por troca em c++ (BUBBLE SORT)

--- java/BubbleSort.java
+++ java/BubbleSort.java
@@ -5,27 +5,27 @@ public class BubbleSort {
 	public static void main(String[] args) {
 
 		int N = 5;
-		int x[] = new int[5];
+		int vetor[] = new int[5];
 		int aux;
 		Scanner entrada = new Scanner(System.in);
 
 		for (int i = 0; i < N; i++) {
 			System.out.print("Digite o " + (i + 1) + "° número: ");
-			x[i] = entrada.nextInt();
+			vetor[i] = entrada.nextInt();
 		}
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < (N - 1); j++) {
-				if (x[j] > x[j + 1]) {
-					aux = x[j];
-					x[j] = x[j + 1];
-					x[j + 1] = aux;
+				if (vetor[j] > vetor[j + 1]) {
+					aux = vetor[j];
+					vetor[j] = vetor[j + 1];
+					vetor[j + 1] = aux;
 				}
 			}
 		}
 
 		for (int i = 0; i < N; i++) {
-			System.out.println((i + 1) + "° número: " + x[i]);
+			System.out.println((i + 1) + "° número: " + vetor[i]);
 		}
 	}
 }
