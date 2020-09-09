4264bc5e1b06422d72fb64117003d37abf4d1441
GabrielIFPB <jeronimogbariel@gmail.com>
2020-09-09 13:35:38 -0300

    algoritmo de ordenação por troca (BUBBLE SORT)

--- java/BubbleSort.java
+++ java/BubbleSort.java
@@ -1,14 +1,31 @@
 
+import java.util.Scanner;
+
 public class BubbleSort {
 	public static void main(String[] args) {
-		System.out.println("Olá, mundo");
 
+		int N = 5;
+		int x[] = new int[5];
+		int aux;
+		Scanner entrada = new Scanner(System.in);
 
-		// int x[] = new int[5];
-		// int aux, n;
+		for (int i = 0; i < N; i++) {
+			System.out.print("Digite o " + (i + 1) + "° número: ");
+			x[i] = entrada.nextInt();
+		}
 
-		// for (int i = 0; i < 5; i++) {
-		// 	x[i] = 
-		// }
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < (N - 1); j++) {
+				if (x[j] > x[j + 1]) {
+					aux = x[j];
+					x[j] = x[j + 1];
+					x[j + 1] = aux;
+				}
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			System.out.println((i + 1) + "° número: " + x[i]);
+		}
 	}
 }
